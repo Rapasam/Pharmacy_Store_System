e951d29f26814707eb5a4ad608dcd45b8c9caa94
Raphael Samaglo <150895822+Rapasam@users.noreply.github.com>
2024-02-10 18:34:59 +0000

    Created Classes for Product, Inventory and Customer

--- management.py
+++ management.py
@@ -1,7 +1,45 @@
-#Function to display products
-def display_products():
-    pass
+#A Class for the product
+class Product:
+    def __init__(self, name, price, quantity, description):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.description = description
 
-#Function to process a sale
-def sale_process():
-    pass
+    def update_quantity(self, new_quantity):
+        self.quantity = new_quantity
+    
+    def get_details(self):
+        return f"{self.name}: {self.description}, ₵{self.price}, {self.quantity} available."
+
+#A Class for taking inventory
+class Inventory:
+    def __init__(self):
+        self.products = []
+    
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+    
+    def search_product(self, name):
+        for product in self.products:
+            if product.name == name:
+                return product
+        return None
+
+#A Class for the Customer
+class Customer:
+    def __init__(self, name, address, contact):
+        self.name = name
+        self.address = address
+        self.contact = contact
+
+    def purchase_product(self, product, quantity):
+        if product.quantity >= quantity:
+            product.update_quantity(product.quantity - quantity)
+            return product.price * quantity
+        else:
+            return 0
+
