9418410d88d8a38fb6f1f48b6014437d00c76bda
Raphael Samaglo <150895822+Rapasam@users.noreply.github.com>
2024-02-12 00:54:23 +0000

    finishing touches added

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from management import *
 # Usage example:
 if __name__ == "__main__":
     # Create products
-    paracetamol = Product("Paracetamol", 8, 100, "Pain relief")
-    ibuprofen = Product("Ibuprofen", 3, 200, "Anti-inflammatory")
+    gebedol_extra = Product("Gebedol Extra", 10, 100, "Pain relief")
+    martins_liver_salt = Product("Martins Liver Salt", 5, 200, "Anti-inflammatory")
 
     # Add products to inventory
     inventory = Inventory()
-    inventory.add_product(paracetamol)
-    inventory.add_product(ibuprofen)
+    inventory.add_product(gebedol_extra)
+    inventory.add_product(martins_liver_salt)
 
     # Create a customer
     customer = Customer("Monica Nyamekye", "Plot 5 Block K, Anwomaso-Kumasi", "monicanyamekye45@gmail.com")
 
     # Process a sale
     sales_software = SalesSoftware(inventory)
-    sales_software.process_sale(customer, ["Paracetamol", "Ibuprofen"], [5, 2], "2024-02-11")
+    sales_software.process_sale(customer, ["Gebedol Extra", "Martins Liver Salt"], [5, 3], "2024-02-11")
 
     # Generate sales report
     sales_software.generate_sales_report()
